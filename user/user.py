3732b7545d271698bf1e605b33c6205de7e67a63
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2025-10-10 07:04:38 -0400

    update to add_user

--- user/user.py
+++ user/user.py
@@ -2,19 +2,29 @@ import sys
 import subprocess
 
 def add_user(username, password):
-        add_user_command = ['useradd', '-p', password, username]
-        subprocess.run(add_user_command)
-        sys.exit(0)
+        add_user_command = ['useradd', '-p', password, '-m', '-g', 'users, mail', username]
+        results = subprocess.run(add_user_command, capture_output=True)
+        if results.returncode > 0:
+                print(f'Had an issue could not create {username}, please check and try agin.')
+                sys.exit(1)   
+        else:
+                print(f'{username} was created successfully!')
+                sys.exit(0)
 
 def remove_user(username):
         remove_user_command = ['deluser', '--remove-all-files', username]
-        subprocess.run(remove_user_command)
-        print(f'${username} was removed')
-        exit(0)
+        results = subprocess.run(remove_user_command, capture_output=True)
+        if results.returncode > 0:
+                print(f'{username} was not found, please try another username.')
+                sys.exit(1)
+        else:
+                print(f'{username} was removed!')
+                sys.exit(0)
 
 def add_user_group(username, group):
-        group_user_command = ['chmod', '-aG', group, username ]
-        subprocess.run(group_user_command)
-        print(f'{username} was added to {group}')
-
-add_user_group('danielortiz', 'users')
+        group_user_command = ['usermod', '-aG', group, username ]
+        results = subprocess.run(group_user_command)
+        if results.returncode > 0:
+                print(f'{username} or {group} could not be found, please check and try again.')
+        else:
+                print(f'{username} was added to {group} successfully!')
