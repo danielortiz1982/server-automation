bc33557f0c59bdfdcea9798c3d2bea977fb7a68e
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2025-10-16 05:24:41 -0400

    update to main project structure

---
+++ env-exports.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SERVER_NAME="server_name"
+export SERVER_IP_ADDRESS="server_ip_address"
+export SERVER_DOMAIN_NAME="example.com"
+export SERVER_ADMIN_USER="username"
+export SERVER_ADMIN_PASSWORD="server_password"
+export SERVER_DB_WP_NAME="server_db"
+
+echo "$SERVER_NAME $SERVER_ADMIN_USER@$SERVER_IP_ADDRESS ENV WAS SUCCESSFULLY!"
