03dc7224e41d3243cfb6405cfe786df9e518ea40
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2025-10-17 13:38:47 -0400

    update to vhost

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# sudo python3 server-dependencies/server-dependencies.py
-# sudo python3 server-db-setup/server-db-setup.py
+sudo python3 server-dependencies/server-dependencies.py
+sudo python3 server-db-setup/server-db-setup.py
 
-### Bind9
-# sudo python3 server-bind9/server-bind9.py
-# sudo sh server-bind9/server-zones.sh
+## Bind9
+sudo python3 server-bind9/server-bind9.py
+sudo sh server-bind9/server-zones.sh
 
-# sudo python3 server-vhost/server-vhost.py
-
+## Apache2 Virtual Host
+sudo python3 server-vhost/server-vhost.py
 sudo sh server-vhost/server-vhost.sh
