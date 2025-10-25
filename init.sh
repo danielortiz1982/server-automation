99f406a85d7d66bf97fd0c4412622260ea318926
searchagents <searchagents.io@gmail.com>
2025-10-25 13:54:20 -0400

    update to bind9 automation

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# sudo sh server-dependencies/server-dependencies.sh
+sudo sh server-dependencies/server-dependencies.sh
 # sudo python server-db-setup/server-db-setup.py
 
 ## Bind9
-sudo python server-bind9/server-bind9.py
-sudo sh server-bind9/server-zones.sh
+sudo python3 server-bind9/server-bind9.py
+# sudo sh server-bind9/server-zones.sh
 
 ## Apache2 Virtual Host
 # sudo python server-vhost/server-vhost.py
--- server-bind9/server-bind9.py
+++ server-bind9/server-bind9.py
@@ -8,7 +8,7 @@ with open('env.json', 'r') as config:
     content = config.read()
     server_config = json.loads(content)
 
-with open('server-bind9/db.your-domain-name.com') as f:
+with open('server-bind9/db.your-domain-name.com', 'r') as f:
     f = f.read()
     f = f.replace('$SERVER_DOMAIN_NAME', server_config['SERVER_DOMAIN_NAME'])
     f = f.replace('$SERVER_IP_ADDRESS', server_config['SERVER_IP_ADDRESS'])
@@ -17,12 +17,12 @@ with open('server-bind9/db.your-domain-name.com') as f:
 with open(f'/etc/bind/db.{server_config["SERVER_DOMAIN_NAME"]}', "w") as f:
     f.write(zone)
     
-with open('server-bind9/server.named.conf.local') as f:
+with open('server-bind9/server.named.conf.local', 'r') as f:
     f = f.read()
     f = f.replace('$SERVER_DOMAIN_NAME', server_config['SERVER_DOMAIN_NAME'])
     named = f
 
-with open('server-bind9/named.conf.local', 'w') as f:
+with open('/etc/bind/named.conf.local', 'w') as f:
     f.write(named)
 
 print('Server Bind9 automation successfully!')
