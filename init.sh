21562d704fa934534965e04ee25c78ccd951153c
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2025-11-01 06:41:41 -0400

    update to saslauthd automation

--- init.sh
+++ init.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# sudo sh server-dependencies/server-dependencies.sh
+sudo sh server-dependencies/server-dependencies.sh
 # sudo python3 server-db-setup/server-db-setup.py
 
 ## Bind9
-# sudo python3 server-bind9/server-bind9.py
+sudo python3 server-bind9/server-bind9.py
 
 ## Apache2 Virtual Host
-# sudo python3 server-vhost/server-vhost.py
+sudo python3 server-vhost/server-vhost.py
 
 ## Postfix
-# sudo python3 server-postfix/server-postfix.py
+sudo python3 server-postfix/server-postfix.py
 
 ## Dovecot
 sudo python3 server-dovecot/server-dovecot.py
 
 ## Saslauthd
-# sudo python3 server-saslauthd/server-saslauthd.py
-# sudo sh server-saslauthd/server-saslauthd.sh
+sudo python3 server-saslauthd/server-saslauthd.py
--- server-saslauthd/server-saslauthd.py
+++ server-saslauthd/server-saslauthd.py
@@ -1,1 +1,30 @@
-print('saslauthd')
+
+import json
+import subprocess
+
+server_config = {}
+smtp = ""
+
+with open('env.json', 'r') as config:
+    content = config.read()
+    server_config = json.loads(content)
+
+with open('server-saslauthd/smtp', 'r') as f:
+    f = f.read()
+    f = f.replace('$SERVER_ADMIN_USER', server_config['SERVER_ADMIN_USER'])
+    f = f.replace('$SERVER_ADMIN_PASSWORD', server_config['SERVER_ADMIN_PASSWORD'])
+    f = f.replace('$SERVER_DB_NAME', server_config['SERVER_DB_NAME'])
+    smtp = f
+
+with open('/etc/pam.d/smtp', 'w') as f:
+    f.write(smtp)
+    subprocess.run("sudo mkdir -p /var/spool/postfix/var/run/saslauthd", shell=True)
+    subprocess.run("sudo cp server-saslauthd/saslauthd /etc/default/saslauthd", shell=True)
+    subprocess.run("sudo cp server-saslauthd/smtpd.conf /etc/postfix/sasl/smtpd.conf", shell=True)
+    subprocess.run("sudo chmod o-rwx /etc/pam.d/smtp", shell=True)
+    subprocess.run("sudo chmod o-rwx /etc/postfix/sasl/smtpd.conf", shell=True)
+    subprocess.run("sudo usermod  -aG sasl postfix", shell=True)
+    subprocess.run("sudo systemctl restart postfix", shell=True)
+    subprocess.run("sudo systemctl restart saslauthd", shell=True)
+
+print("'Server saslauthd automation successfully!'")
