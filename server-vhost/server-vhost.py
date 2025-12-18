816b2ca74b023f6c420d3a81ca245b9d1c5a9793
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2025-12-18 11:01:39 -0500

    update to db setup

--- server-vhost/server-vhost.py
+++ server-vhost/server-vhost.py
@@ -18,12 +18,8 @@ with open('server-vhost/your-domain-name.conf', 'r') as f:
 with open(f'/etc/apache2/sites-available/{server_config["SERVER_DOMAIN_NAME"]}.conf', 'w') as f:
     f.write(conf_file)
 
-subprocess.run('sudo a2dissite 000-default', shell=True)
-subprocess.run(f'sudo a2ensite {server_config["SERVER_DOMAIN_NAME"]}', shell=True)
-subprocess.run(f'sudo systemctl reload apache2', shell=True)
-subprocess.run(f'sudo systemctl restart apache2', shell=True)
+
 subprocess.run(f'sudo mkdir -p /var/www/{server_config["SERVER_DOMAIN_NAME"]}/', shell=True)
-subprocess.run(f'sudo chown www-data: /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
 
 with open('server-vhost/template.html', 'r') as f:
     f = f.read()
@@ -33,6 +29,11 @@ with open('server-vhost/template.html', 'r') as f:
 with open(f'/var/www/{server_config["SERVER_DOMAIN_NAME"]}/index.html', 'w') as f:
     f.write(template)
 
+subprocess.run(f'sudo chown www-data: /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
+subprocess.run('sudo a2dissite 000-default', shell=True)
+subprocess.run(f'sudo a2ensite {server_config["SERVER_DOMAIN_NAME"]}', shell=True)
+subprocess.run(f'sudo systemctl reload apache2', shell=True)
+subprocess.run(f'sudo systemctl restart apache2', shell=True)
 
 ### todo check if client has a wp hosting plan ###
 ### WordPress Automation ###
