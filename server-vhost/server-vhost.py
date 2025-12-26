0fe4b95eaeef90b59b756cfbd0733ef2a6072f5c
searchagents <searchagents.io@gmail.com>
2025-12-26 17:32:22 -0500

    update to vhost

--- server-vhost/server-vhost.py
+++ server-vhost/server-vhost.py
@@ -25,16 +25,17 @@ with open(f'/etc/apache2/sites-available/{server_config["SERVER_DOMAIN_NAME"]}.c
     f.write(conf_file)
 
 ### todo if does not have a wp plan ###
-# with open('server-vhost/template.html', 'r') as f:
-#     f = f.read()
-#     f = f.replace('$SERVER_NAME', server_config['SERVER_NAME'])
-#     template = f
+with open('server-vhost/template.html', 'r') as f:
+    f = f.read()
+    f = f.replace('$SERVER_NAME', server_config['SERVER_NAME'])
+    template = f
 
-# with open(f'server-vhost/index.html', 'w') as f:
-#     f.write(template)
+with open(f'server-vhost/index.html', 'w') as f:
+    f.write(template)
 
 subprocess.run(f'sudo mkdir -p /var/www/{server_config["SERVER_DOMAIN_NAME"]}/', shell=True)
 subprocess.run(f'sudo chown www-data: /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
+subprocess.run(f'sudo -u www-data cp server-vhost/index.html /var/www/{server_config["SERVER_DOMAIN_NAME"]}/index.html')
 subprocess.run('sudo a2dissite 000-default', shell=True)
 subprocess.run(f'sudo a2ensite {server_config["SERVER_DOMAIN_NAME"]}', shell=True)
 subprocess.run(f'sudo systemctl reload apache2', shell=True)
@@ -42,11 +43,11 @@ subprocess.run(f'sudo systemctl restart apache2', shell=True)
 
 ### todo check if client has a wp hosting plan ###
 ### WordPress Automation ###
-subprocess.run(f'curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
-subprocess.run(f'sudo -u www-data cp /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config-sample.php /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
-subprocess.run(f'sudo -u www-data sed -i "s/database_name_here/{server_config["SERVER_DB_NAME"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
-subprocess.run(f'sudo -u www-data sed -i "s/username_here/{server_config["SERVER_ADMIN_USER"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
-subprocess.run(f'sudo -u www-data sed -i "s/password_here/{server_config["SERVER_ADMIN_PASSWORD"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
+# subprocess.run(f'curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
+# subprocess.run(f'sudo -u www-data cp /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config-sample.php /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
+# subprocess.run(f'sudo -u www-data sed -i "s/database_name_here/{server_config["SERVER_DB_NAME"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
+# subprocess.run(f'sudo -u www-data sed -i "s/username_here/{server_config["SERVER_ADMIN_USER"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
+# subprocess.run(f'sudo -u www-data sed -i "s/password_here/{server_config["SERVER_ADMIN_PASSWORD"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
 
 ### Certbot Automation ###
 SHELL_COMMAND=f'sudo certbot run --{WEBSERVER} -d {DOMAIN} -d {WWW_DOMAIN} --noninteractive --agree-tos -m {EMAIL}'
