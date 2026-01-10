9876d10e8c70806e2b73d9a891612110b313e339
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2026-01-10 07:57:59 -0500

    update to init

--- init.sh
+++ init.sh
@@ -10,14 +10,14 @@ sudo python3 server_db/server_db.py
 ## Bind9 Zones ###
 sudo python3 server_bind9/server_bind9.py
 ### Virtual Host ###
-sudo python3 server_vhost/server_vhost.py
+# sudo python3 server_vhost/server_vhost.py
 ### Postfix ###
-sudo python3 server_postfix/server_postfix.py
+# sudo python3 server_postfix/server_postfix.py
 ### Dovecot ###
-sudo python3 server_dovecot/server_dovecot.py
+# sudo python3 server_dovecot/server_dovecot.py
 ### Saslauthd ###
-sudo python3 server_saslauthd/server_saslauthd.py
+# sudo python3 server_saslauthd/server_saslauthd.py
 ### DKIM & DMARC ###
-sudo python3 server_dkim/server_dkim.py
+# sudo python3 server_dkim/server_dkim.py
 ### Exit Message ###
 echo "Server Automation Complete!"
--- server_vhost/server_vhost.py
+++ server_vhost/server_vhost.py
@@ -5,12 +5,6 @@ server_config = {}
 conf_file = ""
 template = ""
 
-### Certbot variables (Modify these) ###
-EMAIL=f'webmaster@{server_config["SERVER_DOMAIN_NAME"]}'                        # Your contact email for urgent renewal notices
-DOMAIN=server_config["SERVER_DOMAIN_NAME"]                                      # Your main domain name
-WWW_DOMAIN=f'www.{server_config["SERVER_DOMAIN_NAME"]}'                         # The www subdomain
-WEBSERVER="apache"                                                              # Or "nginx" if you use Nginx
-
 with open('env.json', 'r') as config:
     content = config.read()
     server_config = json.loads(content)
@@ -48,6 +42,12 @@ subprocess.run(f'sudo systemctl restart apache2', shell=True)
 # subprocess.run(f'sudo -u www-data sed -i "s/database_name_here/{server_config["SERVER_DB_NAME"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
 # subprocess.run(f'sudo -u www-data sed -i "s/username_here/{server_config["SERVER_ADMIN_USER"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
 # subprocess.run(f'sudo -u www-data sed -i "s/password_here/{server_config["SERVER_ADMIN_PASSWORD"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wordpress/wp-config.php', shell=True)
+
+### Certbot variables (Modify these) ###
+EMAIL=f'webmaster@{server_config["SERVER_DOMAIN_NAME"]}'                        # Your contact email for urgent renewal notices
+DOMAIN=server_config["SERVER_DOMAIN_NAME"]                                      # Your main domain name
+WWW_DOMAIN=f'www.{server_config["SERVER_DOMAIN_NAME"]}'                         # The www subdomain
+WEBSERVER="apache"                                                              # Or "nginx" if you use Nginx
 
 ### Certbot Automation ###
 SHELL_COMMAND=f'sudo certbot run --{WEBSERVER} -d {DOMAIN} -d {WWW_DOMAIN} --noninteractive --agree-tos -m {EMAIL}'
