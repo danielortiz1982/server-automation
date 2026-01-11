b4433c4b693c6b7e35a46013e9bced520775bde2
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2026-01-11 09:45:05 -0500

    update to init

--- server_vhost/server_vhost.py
+++ server_vhost/server_vhost.py
@@ -28,18 +28,17 @@ with open(f'server_vhost/index.html', 'w') as f:
 
 subprocess.run(f'sudo mkdir -p /var/www/{server_config["SERVER_DOMAIN_NAME"]}/', shell=True)
 subprocess.run(f'sudo chown www-data: /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
-subprocess.run(f'sudo -u www-data cp server_vhost/index.html /var/www/{server_config["SERVER_DOMAIN_NAME"]}/index.html', shell=True)
+# subprocess.run(f'sudo -u www-data cp server_vhost/index.html /var/www/{server_config["SERVER_DOMAIN_NAME"]}/index.html', shell=True)
 subprocess.run('sudo a2dissite 000-default', shell=True)
 subprocess.run(f'sudo a2ensite {server_config["SERVER_DOMAIN_NAME"]}', shell=True)
 subprocess.run(f'sudo systemctl reload apache2', shell=True)
 subprocess.run(f'sudo systemctl restart apache2', shell=True)
 
-### todo check if client has a wp hosting plan ###
 ### WordPress Automation ###
-# subprocess.run(f'curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
-# subprocess.run(f'sudo -u www-data cp /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config-sample.php /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
-# subprocess.run(f'sudo -u www-data sed -i "s/database_name_here/{server_config["SERVER_DB_NAME"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
-# subprocess.run(f'sudo -u www-data sed -i "s/username_here/{server_config["SERVER_ADMIN_USER"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
-# subprocess.run(f'sudo -u www-data sed -i "s/password_here/{server_config["SERVER_ADMIN_PASSWORD"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
+subprocess.run(f'curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /var/www/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
+subprocess.run(f'sudo -u www-data cp /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config-sample.php /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
+subprocess.run(f'sudo -u www-data sed -i "s/database_name_here/{server_config["SERVER_DB_NAME"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
+subprocess.run(f'sudo -u www-data sed -i "s/username_here/{server_config["SERVER_ADMIN_USER"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
+subprocess.run(f'sudo -u www-data sed -i "s/password_here/{server_config["SERVER_ADMIN_PASSWORD"]}/" /var/www/{server_config["SERVER_DOMAIN_NAME"]}/wp-config.php', shell=True)
 
 print('Vhost automation successfully complete!')
