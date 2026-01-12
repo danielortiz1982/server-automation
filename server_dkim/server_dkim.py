2792290a26884a0780eb3858cbf6259db77a1537
Daniel Ortiz <dortiz@Daniels-Mac-mini.local>
2026-01-12 17:18:03 -0500

    update to init

--- server_dkim/server_dkim.py
+++ server_dkim/server_dkim.py
@@ -2,6 +2,10 @@ import json
 import subprocess
 
 server_config = {}
+keytable = ""
+trustedhosts = ""
+signingtable = ""
+
 
 with open('env.json', 'r') as config:
     content = config.read()
@@ -10,3 +14,28 @@ with open('env.json', 'r') as config:
 subprocess.run(f'sudo mkdir -p /etc/opendkim/keys/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
 subprocess.run(f'sudo opendkim-genkey -s mail -d {server_config["SERVER_DOMAIN_NAME"]} -D /etc/opendkim/keys/{server_config["SERVER_DOMAIN_NAME"]}', shell=True)
 subprocess.run(f'sudo chown -R opendkim:opendkim /etc/opendkim/keys')
+
+with open('server_dkim/template.KeyTable', 'r') as f:
+    f = f.read()
+    f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
+    keytable = f
+
+with open('server_dkim/KeyTable', 'w') as f:
+    f.write(keytable)
+
+with open('server_dkim/template.SigningTable', 'r') as f:
+    f = f.read()
+    f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
+    signingtable = f
+
+with open('server_dkim/SigningTable', 'w') as f:
+    f.write(signingtable)
+
+with open('server_dkim/template.TrustedHosts', 'r') as f:
+    f = f.read()
+    f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
+    f = f.replace("$SERVER_IP_ADDRESS", server_config["SERVER_IP_ADDRESS"])
+    trustedhosts = f
+
+with open('server_dkim/TrustedHosts', 'w') as f:
+    f.write(trustedhosts)
