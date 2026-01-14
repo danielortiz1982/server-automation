dc6cdd9e1e1914e59ec5a864a11f73245f8a34c8
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2026-01-14 11:33:59 -0500

    update to init

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 ### Server Dependencies ###
 sudo sh server_dependencies/server_dependencies.sh
+### Server Network Hosts
+sudo python3 server_hosts/server_hosts.py
 ### Mysql Database ###
 sudo python3 server_db/server_db.py
 ## Bind9 Zones ###
@@ -15,6 +17,6 @@ sudo python3 server_dovecot/server_dovecot.py
 ### Saslauthd ###
 sudo python3 server_saslauthd/server_saslauthd.py
 ### DKIM & DMARC ###
-# sudo python3 server_dkim/server_dkim.py
+sudo python3 server_dkim/server_dkim.py
 ### Exit Message ###
 echo "Server Automation Complete!"
