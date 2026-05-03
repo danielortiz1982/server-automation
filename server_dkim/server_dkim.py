68c176b4f61cf52b646dfb1e7cff4de891ca0845
Daniel Ortiz <dortiz@mac.mynetworksettings.com>
2026-05-03 06:16:50 -0400

    test

--- server_dkim/server_dkim.py
+++ server_dkim/server_dkim.py
@@ -19,22 +19,24 @@ with open('server_dkim/template.KeyTable', 'r') as f:
     f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
     keytable = f
 
-with open('server_dkim/KeyTable', 'w') as f:
-    f.write(keytable)
+print('DKIM Finished')
 
-with open('server_dkim/template.SigningTable', 'r') as f:
-    f = f.read()
-    f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
-    signingtable = f
+# with open('server_dkim/KeyTable', 'w') as f:
+#     f.write(keytable)
 
-with open('server_dkim/SigningTable', 'w') as f:
-    f.write(signingtable)
+# with open('server_dkim/template.SigningTable', 'r') as f:
+#     f = f.read()
+#     f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
+#     signingtable = f
 
-with open('server_dkim/template.TrustedHosts', 'r') as f:
-    f = f.read()
-    f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
-    f = f.replace("$SERVER_IP_ADDRESS", server_config["SERVER_IP_ADDRESS"])
-    trustedhosts = f
+# with open('server_dkim/SigningTable', 'w') as f:
+#     f.write(signingtable)
 
-with open('server_dkim/TrustedHosts', 'w') as f:
-    f.write(trustedhosts)
+# with open('server_dkim/template.TrustedHosts', 'r') as f:
+#     f = f.read()
+#     f = f.replace("$SERVER_DOMAIN_NAME", server_config["SERVER_DOMAIN_NAME"])
+#     f = f.replace("$SERVER_IP_ADDRESS", server_config["SERVER_IP_ADDRESS"])
+#     trustedhosts = f
+
+# with open('server_dkim/TrustedHosts', 'w') as f:
+#     f.write(trustedhosts)
